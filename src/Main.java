625fe080a05418271a5318d540307a8e92b2da4f
MaxKR1994 <98046645+MaxKR1994@users.noreply.github.com>
2022-04-21 20:15:25 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+class Main {
+    public static void Store(){
+        System.out.println("Welcome to our store!");
+    }
+
+
+    public static void main(String[] args) {
+
+        Phone IphoneX = new Phone("Black","IOS",2000,5,64);
+        Phone Iphone13 = new Phone("Silver","IOS",3000,6,512);
+        Phone SamsungS20 = new Phone("Blue","android",4000,6,256);
+        Phone Pixel6 = new Phone("White","android",4500,6,128);
+        Phone Xiaomi12 = new Phone("Red","android",5000,6,32);
+        Phone Huawei = new Phone();
+        Phone.stock = "In stock:";
+
+        IphoneX.setColor("SpaceGrey");
+        IphoneX.setOperatingSystem("IOS");
+        IphoneX.setBatteryCapacity(2500);
+        IphoneX.setScreenSize(6);
+        IphoneX.setMemory(512);
+
+        Store();
+        System.out.println(Phone.stock);
+        System.out.println(IphoneX.toString());
+        System.out.println(Iphone13.toString());
+        System.out.println(SamsungS20.toString());
+        System.out.println(Pixel6.toString());
+        System.out.println(Xiaomi12.toString());
+        System.out.println(Huawei.toString());
+        System.out.println(SamsungS20.getMemory());
+
+    }
+
+}
---
+++ src/Phone.java
@@ -0,0 +1,82 @@
+public class Phone {
+    private String Color;
+    private String OperatingSystem;
+    private Integer BatteryCapacity;
+    private Integer ScreenSize;
+    private Integer Memory;
+
+    public static String stock;
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setColor(String color) {
+        Color = color;
+    }
+
+    public String getOperatingSystem() {
+        return OperatingSystem;
+    }
+
+    public void setOperatingSystem(String operatingSystem) {
+        OperatingSystem = operatingSystem;
+    }
+
+    public Integer getBatteryCapacity() {
+        return BatteryCapacity;
+    }
+
+    public void setBatteryCapacity(Integer batteryCapacity) {
+        BatteryCapacity = batteryCapacity;
+    }
+
+    public Integer getScreenSize() {
+        return ScreenSize;
+    }
+
+    public void setScreenSize(Integer screenSize) {
+        ScreenSize = screenSize;
+    }
+
+    public Integer getMemory() {
+        return Memory;
+    }
+
+    public void setMemory(Integer memory) {
+        Memory = memory;
+    }
+
+    public Phone(String color, String operatingSystem, Integer batteryCapacity, Integer screenSize, Integer memory) {
+        this.Color = color;
+        this.OperatingSystem = operatingSystem;
+        this.BatteryCapacity = batteryCapacity;
+        this.ScreenSize = screenSize;
+        this.Memory = memory;
+    }
+
+    public Phone() {
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "Color='" + Color + '\'' +
+                ", OperatingSystem='" + OperatingSystem + '\'' +
+                ", BatteryCapacity=" + BatteryCapacity +
+                ", ScreenSize=" + ScreenSize +
+                ", Memory=" + Memory +
+                '}';
+    }
+
+    public String toString(String color, String operatingSystem, Integer batteryCapacity, Integer screenSize, Integer memory) {
+        return "Phone{" +
+                "Color='" + Color + '\'' +
+                ", OperatingSystem='" + OperatingSystem + '\'' +
+                ", BatteryCapacity=" + BatteryCapacity +
+                ", ScreenSize=" + ScreenSize +
+                ", Memory=" + Memory +
+                '}';
+    }
+
+}
